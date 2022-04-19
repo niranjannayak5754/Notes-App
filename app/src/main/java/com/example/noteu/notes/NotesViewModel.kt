bdd425aac9322f4bb220da1ccc38a648cac1a46b
Niranjan Nayak <niranjannayak0717@gmail.com>
2022-04-19 07:58:26 +0530

    1.6 --> Creating a ViewModel for the activity

--- app/src/main/java/com/example/noteu/notes/NotesViewModel.kt
+++ app/src/main/java/com/example/noteu/notes/NotesViewModel.kt
@@ -3,14 +3,26 @@ package com.example.noteu.notes
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class NotesViewModel(application: Application): AndroidViewModel(application){
     val allNotes : LiveData<List<Notes>>
+    private val repository : NotesRepository
 
     init {
-        val dao = NotesDatabase.getDatabase(application).notesDAO()
-        val repository = NotesRepository(dao)
+        val dao = NotesDatabase.getDatabase(application).getNotesDAO()
+        repository = NotesRepository(dao)
         allNotes = repository.allNotes
+    }
+
+    fun deleteNote(note: Notes) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(note)
+    }
+
+    fun insertNote(note: Notes) = viewModelScope.launch (Dispatchers.IO){
+        repository.insert(note)
     }
 
 }
